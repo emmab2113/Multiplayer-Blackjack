5e12cc33a8690b3f44dac856697a1ec3fdc7c7d5
Michael-Eric Seijo <miceriseij@gmail.com>
2026-04-28 02:02:13 -0700

    Adding initial Card.java

---
+++ src/MBServer/Card.java
@@ -0,0 +1,20 @@
+public class Card {
+	
+	// Fields:
+	private String rank;
+	private String suit;
+	
+	public Card(String cardRank, String cardSuit) {
+		this.rank = cardRank;
+		this.suit = cardSuit;
+	}
+	
+	public String getRank() {
+		return this.rank;
+	}
+	
+	public String getSuit() {
+		return this.rank;
+	}
+	
+}
