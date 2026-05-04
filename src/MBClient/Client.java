74a3d02717e02bca1e3d82bd7b58d059f7544984
emmab2113 <emmab2113@gmail.com>
2026-05-03 20:35:00 -0700

    small updates

--- src/MBClient/Client.java
+++ src/MBClient/Client.java
@@ -14,8 +14,17 @@ import enums.ErrorType;
 import enums.MessageStatus;
 import enums.MessageType;
 
+/*
+ * CLIENT CLASS:
+ * Acts as handle for a users connectivity to the server. Holds both network information 
+ * and user information. Is not a driver, therefore must be activated externally.
+ *
+ */
+
 public class Client {
-	// network connectivity //
+	
+	// ===== NETWORK INFO ====== //
+	
 	private int port;
 	private String host;
 	private Socket socket;
@@ -23,18 +32,21 @@ public class Client {
 	private InputStream inputStream;
 	private ObjectOutputStream objectOutputStream;
 	private ObjectInputStream objectInputStream;	
-	// user info //
+	
+	// ======= USER INFO ======= //
+	
 	private String IPAddress; // client IP or server IP?
 	private Vector<Message> messageLog; // supposed to be recording every message?
 	private boolean loggedIn;
 	private String username;
 	private boolean[] seatedPlayers;
 	
-	// CONSTRUCTORS //
+	// ====== CONSTRUCTORS ===== //
+	
 	public Client() {
 		try {
 			// initialize port/host with default values
-			this.port =1234;
+			this.port = 1234;
 			this.host = InetAddress.getLocalHost().getHostAddress().trim(); // host default to inet address;
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -45,7 +57,8 @@ public class Client {
 		this.host = host;
 	}
 	
-	// PUBLIC METHODS //
+	// ===== PUBLIC METHODS ==== //
+	
 	public void connect() {
 		try {		
 	        // connect to the ServerSocket at host:port
@@ -54,22 +67,22 @@ public class Client {
 	        
 	        // establish output stream to server
 	        // get output stream from connected socket
-	        OutputStream outputStream = socket.getOutputStream();
+	        outputStream = socket.getOutputStream();
 	        // create an ObjectOutputStream to send an object through outputStream
 			objectOutputStream = new ObjectOutputStream(outputStream);
 			
 	        // establish input stream from server
 	        // get input stream from connected socket
-	        InputStream inputStream = socket.getInputStream();
+	        inputStream = socket.getInputStream();
 	        // create an ObjectInputStream to receive an object through inputStream
-	        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+	        objectInputStream = new ObjectInputStream(inputStream);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	public boolean verifyConnection() {
-        // send Connected message to server
-        send(new Message(MessageType.Connected, MessageStatus.Pending));
+		// send Connected message to server
+        send(new Message(MessageType.Connected, MessageStatus.Pending)); 
         
         // read response from server
         Message responseMessage = receive();
@@ -88,7 +101,8 @@ public class Client {
 	}
 	public boolean requestLogIn(String username, String password, String credentials) {
 		// send LogIn message to server
-    	send(new Message(MessageType.LogIn, MessageStatus.Pending, username + "," + password + "," + credentials));
+    	send(new Message(MessageType.LogIn, MessageStatus.Pending, 
+    					 username + "," + password + "," + credentials));
     	
     	// read response from server
     	Message responseMessage = receive();
@@ -96,6 +110,8 @@ public class Client {
     	// interpret response: verify successful login
     	if (responseMessage.getType() == MessageType.LogIn 
     		&& responseMessage.getStatus() == MessageStatus.Success) {
+    		this.loggedIn = true;
+    		this.username = username;
     		return true;
     	}
     	else {
@@ -104,7 +120,8 @@ public class Client {
 	}
 	public boolean requestRegistry(String username, String password, String credentials) {
 		// send Register message to server
-    	send(new Message(MessageType.Register, MessageStatus.Pending, username + "," + password + "," + credentials));
+    	send(new Message(MessageType.Register, MessageStatus.Pending, 
+    					 username + "," + password + "," + credentials));
     	
     	// read response message from server
     	Message responseMessage = receive();
@@ -112,6 +129,8 @@ public class Client {
     	// interpret response: verify successful registry
     	if (responseMessage.getType() == MessageType.Register 
     		&& responseMessage.getStatus() == MessageStatus.Success) {
+    		this.loggedIn = true;
+    		this.username = username;
     		return true;
     	}
     	else {
@@ -128,6 +147,7 @@ public class Client {
     	// interpret response: verify successful logout
     	if (responseMessage.getType() == MessageType.LogOut 
     		&& responseMessage.getStatus() == MessageStatus.Success) {
+    		this.loggedIn = true;
     		return true;
     	}
     	else {
@@ -153,7 +173,8 @@ public class Client {
 	}	
 	public double requestBalanceDeposit(Double currency) {
 		// send DepositRequest message to server
-		send(new Message(MessageType.DepositRequest, MessageStatus.Pending, String.valueOf(currency)));
+		send(new Message(MessageType.DepositRequest, MessageStatus.Pending, 
+						 String.valueOf(currency)));
 		
         // read response message from server
 		Message responseMessage = receive();
@@ -170,7 +191,8 @@ public class Client {
 	}
 	public double requestBalanceWithdrawal(Double currency) {
 		// send WithdrawRequest message to server
-		send(new Message(MessageType.WithdrawRequest, MessageStatus.Pending, String.valueOf(currency)));
+		send(new Message(MessageType.WithdrawRequest, MessageStatus.Pending, 
+						 String.valueOf(currency)));
 		
         // read response message from server
 		Message responseMessage = receive();
@@ -238,7 +260,8 @@ public class Client {
 	}
 	public boolean chooseBet(double bet) {
 		// send Bet message to server
-		send(new Message(MessageType.Bet, MessageStatus.Pending, String.valueOf(bet)));
+		send(new Message(MessageType.Bet, MessageStatus.Pending, 
+						 String.valueOf(bet)));
 		
         // read response message from server
 		Message responseMessage = receive();
@@ -261,7 +284,8 @@ public class Client {
 		send(new Message(MessageType.Stand, MessageStatus.Pending));
 	}
 	
-	// PRIVATE METHODS // 
+	// ==== PRIVATE METHODS ==== //
+	
 	private void send(Message msg) {
 		try {
 			objectOutputStream.writeObject(msg);
@@ -274,7 +298,6 @@ public class Client {
 		try {
 			msg = (Message) objectInputStream.readObject();
 		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return msg;
