ebf9a4873d0c2492064d3b207955081c3cbdc197
emmab2113 <emmab2113@gmail.com>
2026-05-03 17:14:55 -0700

    created scenes that implement Scene interface

---
+++ src/MBClient/scenes/OpenScene.java
@@ -0,0 +1,36 @@
+package MBClient.scenes;
+
+import MBClient.Client;
+import MBClient.GUI;
+import MBClient.Scene;
+
+public class OpenScene implements Scene {
+	private GUI gui;
+	private Client client;
+	
+	// ====== CONSTRUCTOR ====== //
+	
+	public OpenScene(GUI gui, Client client) {
+		this.gui = gui;
+		this.client = client;
+	}
+	
+	// ===== PUBLIC METHODS ==== //
+	
+	public void construct() {
+		
+	}
+	public void destruct() {	
+		
+	}
+	public void render() {
+		
+	}	
+	public void update() {
+		
+	}
+	public void handleInput() {
+		
+	}
+
+}
