c1afcd4ed42d27d89272867bf4f4211f5db225af
emmab2113 <emmab2113@gmail.com>
2026-05-05 10:08:50 -0700

    formatting set

--- src/MBClient/scenes/DealerTableScene.java
+++ src/MBClient/scenes/DealerTableScene.java
@@ -5,15 +5,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import MBClient.Client;
 import MBClient.GUI;
 import MBClient.Scene;
 
-public class PlayerTableScene implements Scene {
+public class DealerTableScene implements Scene {
 	private GUI gui;
 	private Client client;
 	
@@ -21,11 +19,11 @@ public class PlayerTableScene implements Scene {
 	
 	// ====== CONSTRUCTOR ===== //
 	
-	public PlayerTableScene(GUI gui, Client client) {
+	public DealerTableScene(GUI gui, Client client) {
 		this.gui = gui;
 		this.client = client;
 		
-		basePanel = new JPanel();
+basePanel = new JPanel();
 		
 		basePanel.setLayout(new GridLayout(1, 3));
 		
@@ -37,6 +35,19 @@ public class PlayerTableScene implements Scene {
 		
 		JPanel otherPlayerPanel1 = new JPanel();
 		JPanel otherPlayerPanel2 = new JPanel();
+		JPanel otherPlayerPanel3 = new JPanel();
+
+		leftPanel.add(otherPlayerPanel1);
+		leftPanel.add(otherPlayerPanel2);
+		leftPanel.add(otherPlayerPanel3);
+		
+		// center panel
+		
+		JPanel centerPanel = new JPanel();
+		
+		centerPanel.setLayout(new GridLayout(2, 1));
+		
+		JPanel dealerPanel = new JPanel();
 		JPanel buttonPanel = new JPanel();
 		
 		JButton hitButton = new JButton("hit");
@@ -59,21 +70,8 @@ public class PlayerTableScene implements Scene {
 			}
 		});
 		
-		leftPanel.add(otherPlayerPanel1);
-		leftPanel.add(otherPlayerPanel2);
-		leftPanel.add(buttonPanel);
-		
-		// center panel
-		
-		JPanel centerPanel = new JPanel();
-		
-		centerPanel.setLayout(new GridLayout(2, 1));
-		
-		JPanel dealerPanel = new JPanel();
-		JPanel thisPlayerPanel = new JPanel();
-		
 		centerPanel.add(dealerPanel);
-		centerPanel.add(thisPlayerPanel);
+		centerPanel.add(buttonPanel);
 		
 		// right panel
 		
@@ -81,9 +79,9 @@ public class PlayerTableScene implements Scene {
 		
 		rightPanel.setLayout(new GridLayout(3, 1));
 
-		JPanel otherPlayerPanel3 = new JPanel();
 		JPanel otherPlayerPanel4 = new JPanel();
 		JPanel otherPlayerPanel5 = new JPanel();
+		JPanel otherPlayerPanel6 = new JPanel();
 		
 		rightPanel.add(otherPlayerPanel3);
 		rightPanel.add(otherPlayerPanel4);
@@ -92,7 +90,6 @@ public class PlayerTableScene implements Scene {
 		basePanel.add(leftPanel);
 		basePanel.add(centerPanel);
 		basePanel.add(rightPanel);
-
 	}
 	
 	// ===== PUBLIC METHODS ==== //
